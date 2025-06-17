0c48ab35a803af2936cef5cd9a30109428fa2e4c
Mateue Laurani <mateus.laurani.silva@escola.pr.gov.br>
2025-06-17 11:14:42 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
 criaCartao(
     'Sobre anime',
     'Qual o personagem mais foda dos animes',
-    'Joseph Joestar (part 2)'
+    'Joseph Joestar (part 2)',
     
     criaCartao(
     'sobre xaruto',
     'Qual o personagem mais maconheiro do anime?',
-    'Exatamente é o Rock Lee'
+    'Exatamente é o Rock Lee',
 )
 
 criaCartao(
     'Sobre animações',
     'Qual o personagem mais forte das animações?',
-    'exatamente o SuperBoy Prime'
+    'exatamente o SuperBoy Prime',
 )
 
 criaCartao(
     'Minecraft',
     'Qual e o item mais raro de se obter?',
-    'Uma namorada'
+    'Uma namorada',
 )
